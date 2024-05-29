fc3f76b24ea48e9b2e5223e2e6114003f6784c39
zeroskill2400 <zeroskill2400@gmail.com>
2024-05-29 11:04:24 +0900

    bugFix: api 주소 수정
    회원가입 api 주소 수정

--- src/main/java/com/zeroskill/buytopia/controller/MemberController.java
+++ src/main/java/com/zeroskill/buytopia/controller/MemberController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/member")
+@RequestMapping("/api/v1/members")
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
